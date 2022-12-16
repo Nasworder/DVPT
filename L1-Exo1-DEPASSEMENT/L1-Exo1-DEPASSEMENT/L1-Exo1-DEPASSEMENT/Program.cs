1de57f1e2e8feefe1670a86dcd24ccd4192d2e95
Adriano José Queirós da Silva <adriano.queiros@solmani.ch>
2022-12-16 21:25:37 +0100

    Exercice 1 dépassement avec écriture dans les logs

---
+++ L1-Exo1-DEPASSEMENT/L1-Exo1-DEPASSEMENT/L1-Exo1-DEPASSEMENT/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics;
+
+namespace L1_Exo1_DEPASSEMENT
+{
+ 
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float valeurFinale = 0;
+            Console.WriteLine(Traitement(10, 1000, out valeurFinale));
+            Console.WriteLine(valeurFinale);
+
+
+        }
+
+        private static bool Traitement(int nbIteration, float valeurDepart, out float valeurFinale)
+        {
+            try
+            {
+                for (int i = 0; i < nbIteration; i++)
+                {
+                    valeurDepart *= 2;
+                }
+
+                valeurFinale = valeurDepart;
+                return (!float.IsNaN(valeurFinale) && !float.IsInfinity(valeurFinale));
+            }
+            catch (OverflowException e)
+            {
+                EventLog eventLog = new EventLog();
+                eventLog.Source = "Application";
+                eventLog.WriteEntry(e.Message, EventLogEntryType.Error, 1002, 1);
+                
+                valeurFinale = valeurDepart;
+                return false;
+            }
+        }
+
+
+
+
+
+
+    }
+}
