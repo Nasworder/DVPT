4df28fef56bd1ef85018c03695882dabe6318a9a
Adriano José Queirós da Silva <adriano.queiros@solmani.ch>
2022-12-17 14:13:55 +0100

    L2-Exo3-SINGLETON

---
+++ L2-Exo3-SINGLETON/L2-Exo3-SINGLETON/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace L2_Exo3_SINGLETON
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private TheClass theClass;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void idCreateInstance_Click(object sender, RoutedEventArgs e)
+        {
+            string actualRandom = RandomString();
+            lblGenKey.Content = actualRandom;
+            theClass = TheClass.Instance(actualRandom);
+            lblCurrentInstKey.Content = theClass.Value;
+
+        }
+
+        // Return random String of 10 characters
+        private string RandomString()
+        {
+            Random random = new Random();
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, 10)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+        private void idCreateInstance2_Click(object sender, RoutedEventArgs e)
+        {
+            string actualRandom = RandomString();
+            lblGenKey.Content = actualRandom;
+            theClass = TheClass.Instance(actualRandom);
+            lblCurrentInstKey.Content = theClass.Value;
+        }
+    }
+
+}
---
+++ L2-Exo3-SINGLETON/L2-Exo3-SINGLETON/TheClass.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L2_Exo3_SINGLETON
+{
+    internal sealed class TheClass
+    {
+        private static TheClass _instance;
+        private static readonly object _lock = new object();
+        
+        // Permet de vérifier que cela fonctionne correctement
+        public string Value { get; set; }
+
+        private TheClass()
+        {
+            
+        }
+
+        public static TheClass Instance(string value)
+        {
+            // Permet de gérer le multi-thread
+            lock (_lock)
+            {
+                if (_instance == null)
+                {
+                    _instance = new TheClass();
+                    _instance.Value = value;
+                }
+                return _instance;
+            }
+           
+        }
+    }
+}
