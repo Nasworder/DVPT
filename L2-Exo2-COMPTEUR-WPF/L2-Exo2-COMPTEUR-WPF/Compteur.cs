978e8860b9e1323662c504487a6c095e0b17bb7c
Adriano José Queirós da Silva <adriano.queiros@solmani.ch>
2022-12-17 13:08:41 +0100

    L2-Exo2-Compteur

---
+++ L2-Exo2-COMPTEUR-WPF/L2-Exo2-COMPTEUR-WPF/Compteur.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L2_Exo2_COMPTEUR_WPF
+{
+    internal class Compteur
+    {
+        public static int NbObj { get; private set; } = 0;
+
+        public Compteur()
+        {
+            NbObj++;
+        }
+
+        // Destructor -> a la destruction de l'instance
+        ~Compteur()
+        {
+            NbObj--;
+        }
+    }
+}
---
+++ L2-Exo2-COMPTEUR-WPF/L2-Exo2-COMPTEUR-WPF/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace L2_Exo2_COMPTEUR_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        List<Compteur> compteurList;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            compteurList = new List<Compteur>();
+        }
+
+        private void btnInst_Click(object sender, RoutedEventArgs e)
+        {
+            compteurList.Add(new Compteur());
+        }
+
+        private void btnDisplayNbObj_Click(object sender, RoutedEventArgs e)
+        {
+            lblNbObj.Content = Compteur.NbObj;
+        }
+    }
+}
