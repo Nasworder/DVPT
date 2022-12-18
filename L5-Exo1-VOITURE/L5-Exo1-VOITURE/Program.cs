bf86de0151cd11be02317b4949bb4567f4fd8d2f
Adriano José Queirós da Silva <adriano.queiros@solmani.ch>
2022-12-18 15:33:17 +0100

    L5 exo1 voiture

---
+++ L5-Exo1-VOITURE/L5-Exo1-VOITURE/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace L5_Exo1_VOITURE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Voiture v = new Voiture();
+            v.Accelerer(10);
+
+            Console.WriteLine("Vitesse courante: " + v.VitesseCourante);
+
+        }
+    }
+}
---
+++ L5-Exo1-VOITURE/L5-Exo1-VOITURE/Voiture.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L5_Exo1_VOITURE
+{
+    internal class Voiture
+    {
+        public double VitesseCourante { get; private set; }
+
+        public Voiture()
+        {
+            VitesseCourante = 0;
+        }
+
+        public void Accelerer(double vitesse)
+        {
+            VitesseCourante += vitesse;
+        }
+    }
+}
