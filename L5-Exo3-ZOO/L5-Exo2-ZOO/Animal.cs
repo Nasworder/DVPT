dbc928edf4625dafdceeaeaa6253bf53ab85a8a6
Adriano José Queirós da Silva <adriano.queiros@solmani.ch>
2022-12-18 16:06:45 +0100

    Zoo version generique (exo3)

---
+++ L5-Exo3-ZOO/L5-Exo2-ZOO/Animal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L5_Exo2_ZOO
+{
+    internal class Animal
+    {
+        public string nom { get; private set; }
+        public string nourriture { get; private set; }
+
+        public Animal(string nom, string nourriture)
+        {
+            this.nom = nom;
+            this.nourriture = nourriture;
+        }
+    }
+}
---
+++ L5-Exo3-ZOO/L5-Exo2-ZOO/Zoo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L5_Exo2_ZOO
+{
+    internal class Zoo
+    {
+        List<Animal> animaux;
+
+        public Zoo()
+        {
+            animaux = new List<Animal>();
+        }
+
+        public void AjouterAnimal(Animal a)
+        {
+            animaux.Add(a);
+        }
+
+        // Indexeur
+        public string this[string a] {
+            get
+            {
+                foreach (Animal animal in animaux)
+                {
+                    if (animal.nom == a)
+                        return animal.nourriture;
+                }
+                return "Animal non trouvé";
+            }
+
+        }
+    }
+}
